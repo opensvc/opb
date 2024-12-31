1916b030f4c1333737874e37afc6f746a4569966
OPB <devel@opensvc.com>
2024-12-31 05:43:48 +0000

    Add rundeck package builder script

---
+++ docker/rundeck-pkg.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+OPBDOCKER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+OPBROOT="${OPBDOCKER}/.."
+
+cd ${OPBDOCKER} || exit 1
+
+. ${OPBROOT}/environment.sh
+
+[[ -z $NAME ]] && {
+	echo "$0: variable NAME is not defined"
+	exit 1
+}
+
+LREPO=${REPOS[$NAME]}
+
+echo "NAME=$NAME"
+echo "CODE=$CODE"
+echo "LREPO=$LREPO"
+echo
+
+[[ -z $LREPO ]] && {
+	echo "$0: package repository LREPO is not defined"
+	exit 1
+}
+
+docker run -e OSVC_CODE_TO_BUILD=${CODE} -e OSVCDIST=${NAME} -e OSVCREPO=$LREPO -v ${OPBROOT}/tools:/tools --rm $NAME:pkgbuild build || {
+    echo "$0: error while trying to build package"
+    exit 1
+}
