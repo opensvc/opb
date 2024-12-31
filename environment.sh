7d9873ac8c15eecb62edbf31d79bdf2f7d14ca9f
OPB <devel@opensvc.com>
2024-12-31 05:16:22 +0000

    Update repo names

--- environment.sh
+++ environment.sh
@@ -1,16 +1,16 @@
-DISTROS=("rhel7" "rhel8" "rhel9" "sles15" "u2204" "debian12")
+DISTROS=("rhel7" "rhel8" "rhel9" "sles15" "u2004" "u2204" "u2404" "debian11" "debian12")
 # does not work for sles12
 # sles12 container image requires running on a sles12 registered system
 # to act as a proxy to the rpm repositories
 
-ARCH=$(arch)
-
-declare -A REPOS=( [rhel7]=opensvc-3-rhel7-$ARCH-dev
-	           [rhel8]=opensvc-3-rhel8-$ARCH-dev
-	           [rhel9]=opensvc-3-rhel9-$ARCH-dev
-	           [sles15]=opensvc-3-sles15-$ARCH-dev
-	           [u2004]=opensvc-3-ubuntu2004-$ARCH-dev
-	           [u2204]=opensvc-3-ubuntu2204-$ARCH-dev
-	           [u2404]=opensvc-3-ubuntu2404-$ARCH-dev
-	           [debian12]=opensvc-3-bookworm-$ARCH-dev
+declare -A REPOS=( [rhel7]=dev-opensvc-v3-rhel7
+	           [rhel8]=dev-opensvc-v3-rhel8
+	           [rhel9]=dev-opensvc-v3-rhel9
+	           [sles12]=dev-opensvc-v3-sles12
+	           [sles15]=dev-opensvc-v3-sles15
+	           [u2004]=dev-opensvc-v3-focal
+	           [u2204]=dev-opensvc-v3-jammy
+	           [u2404]=dev-opensvc-v3-noble
+	           [debian11]=dev-opensvc-v3-bullseye
+	           [debian12]=dev-opensvc-v3-bookworm
 	         )
