200a53c3486894fbc54c00f09b54b544c22b6dd5
OPB <devel@opensvc.com>
2025-08-11 15:34:40 +0000

    Add debian13

--- environment.sh
+++ environment.sh
@@ -1,4 +1,4 @@
-DISTROS=("rhel7" "rhel8" "rhel9" "sles15" "u2004" "u2204" "u2404" "debian11" "debian12")
+DISTROS=("rhel7" "rhel8" "rhel9" "rhel10" "sles15" "u2004" "u2204" "u2404" "debian12" "debian13")
 # does not work for sles12
 # sles12 container image requires running on a sles12 registered system
 # to act as a proxy to the rpm repositories
@@ -6,11 +6,12 @@ DISTROS=("rhel7" "rhel8" "rhel9" "sles15" "u2004" "u2204" "u2404" "debian11" "de
 declare -A REPOS=( [rhel7]=dev-opensvc-v3-rhel7
 	           [rhel8]=dev-opensvc-v3-rhel8
 	           [rhel9]=dev-opensvc-v3-rhel9
+	           [rhel10]=dev-opensvc-v3-rhel10
 	           [sles12]=dev-opensvc-v3-sles12
 	           [sles15]=dev-opensvc-v3-sles15
 	           [u2004]=dev-opensvc-v3-focal
 	           [u2204]=dev-opensvc-v3-jammy
 	           [u2404]=dev-opensvc-v3-noble
-	           [debian11]=dev-opensvc-v3-bullseye
+	           [debian13]=dev-opensvc-v3-trixie
 	           [debian12]=dev-opensvc-v3-bookworm
 	         )
